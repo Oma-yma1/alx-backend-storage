757d31c96f7b618b835475517fe66d6cef5d9431
Oma-yma1 <zgani.omayma@gmail.com>
2024-01-18 19:08:31 +0000

    task

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,16 +1,18 @@
 -- SQL script that creates a stored procedure ComputeAverageWeightedScoreForUsers
+
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 DELIMITER //
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-  UPDATE users AS Us, 
-    (SELECT Us.id, SUM(score * weight) / SUM(weight) AS we_avg 
-    FROM users AS Us 
-    JOIN corrections as Co ON U.id=C.user_id 
-    JOIN projects AS Pp ON Co.project_id=Pp.id 
-    GROUP BY Us.id)
-  AS WAt
-  SET Us.average_score = WAt.we_avg 
-  WHERE Us.id=WAt.id;
-END //
-DELIMITER;
+    UPDATE users AS Us,
+        (SELECT Us.id, SUM(score * weight) / SUM(weight) AS w_avg
+        FROM users AS Us
+        JOIN corrections as Co ON Us.id=Co.user_id
+        JOIN projects AS Pr ON Co.project_id=Pr.id
+        GROUP BY Us.id)
+    AS WAt
+    SET Us.average_score = WAt.w_avg
+    WHERE Us.id=WAt.id;
+END
+//
+DELIMITER ;
