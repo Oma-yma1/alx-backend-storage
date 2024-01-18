cb36cb7863d3b590664577f6056575efbaae65a4
Oma-yma1 <zgani.omayma@gmail.com>
2024-01-18 18:13:03 +0000

    task

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,12 +1,18 @@
 -- SQL script that creates a stored procedure ComputeAverageScoreForUser
 
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
-DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser(
-  IN user_id INT)
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(user_id INT)
 BEGIN
-  DECLARE avg_score FLOAT;
-  SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
-  UPDATE users SET average_score = avg_score WHERE id=user_id;
+    DECLARE average_score FLOAT;
+    SET average_score = (
+        SELECT AVG(score) 
+        FROM corrections 
+        AS C 
+        WHERE C.user_id=user_id
+    );
+    UPDATE users 
+    SET average_score = average_score 
+    WHERE id=user_id;
 END $$
 DELIMITER ;
