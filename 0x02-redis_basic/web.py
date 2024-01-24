a4b8fcffe868ec9eeb4951ddf1acf3d9b0a7e0a7
Oma-yma1 <zgani.omayma@gmail.com>
2024-01-24 16:42:27 +0000

    taskweb

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -16,12 +16,12 @@ def data_cacher(method: Callable) -> Callable:
     def wrapr(url) -> str:
         """wrapper function"""
         redis_store.incr(f'count:{url}')
-        res = redis_store.get(f'result:{url}')
-        if res:
-            return res.decode('utf-8')
-        res = method(url)
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
         redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, res)
+        redis_store.setex(f'result:{url}', 10, result)
         return result
     return wrapr
 
